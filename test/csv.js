fe0628bfff1b714a42e305f43bf24ce508d6a467
Alex J Ray <alexjamesray@gmail.com>
2017-01-05 11:33:28 +0900

    Adds csv filesToCSV test

--- test/csv.js
+++ test/csv.js
@@ -8,28 +8,48 @@ import {
 
 describe('CSV', function () {
 
+  const csvPrimary = {
+    headers: ['Foo', 'Bar'],
+    rows: [['baz', 4], ['qaz', 5]]
+  };
+
+  const expectedPrimaryCSVContent = 'Foo,Bar\nbaz,4\nqaz,5\n';
+
+  const csvSecondary = {
+    headers: ['Baz', 'Qaz'],
+    rows: [['foo', 'bar'], [3, 4]]
+  };
+
+  const expectedSecondaryCSVContent = 'Bar,Qaz\nfoo,bar\n3,4\n';
+
   describe('#filesToCSV()', function () {
-    it('Should map headers to csv array', function () {
+    const csvFolder = {
+      folder: 'csvs',
+      files: [{
+        file: 'csv_primary.csv',
+        content: csvPrimary
+      }, {
+        file: 'csv_secondary.csv',
+        content: csvSecondary
+      }]
+    };
+
+
+    let files = [csvFolder];
+    let csvFiles = filesToCSV(files);
+
+    it('Should convert files CSV strings', function () {
+
+      console.log(csvFiles);
+
       // assert.equal();
     });
   });
 
   describe('#toCSV', function () {
-
     it('Should convert headers and rows into CSV string', function () {
-      let headers = ['Foo', 'Bar'];
-      let rows    = [
-        ['qaz',3],
-        ['baz',2]
-      ];
-
-      let csvContent = toCSV(headers, rows);
-
-      let expectedString = 'Foo,Bar\nqaz,3\nbaz,2\n';
-
-      console.log(csvContent);
-
-      assert.equal(csvContent, expectedString);
+      let csvContent = toCSV(csvPrimary.headers, csvPrimary.rows);
+      assert.equal(csvContent, expectedPrimaryCSVContent);
     });
   });
 });
